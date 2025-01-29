dd1e548612f1b3fc13e732b835b861ec5701fba0
Caique <caiquetrivelato@outlook.com>
2025-01-29 15:50:36 -0600

    Create mysql_class.py

---
+++ dashboard_script/mysql_class.py
@@ -0,0 +1,24 @@
+"""
+Class designed to hold mysql connection parameters.
+
+Developed by Caique Trivelato
+Version 1.0 
+Date: 01/29/2025
+"""
+
+
+class MySQLConnector:
+    """Handles connection to the MySQL database"""
+    
+    def __init__(self, db_config: Dict[str, Any]):
+        try:
+            self.connection = mysql.connector.connect(**db_config)
+            print("Successfully connected to MySQL database")
+        except Exception as e:
+            print(f"Error connecting to MySQL database: {str(e)}")
+            raise
+    
+    def get_connection(self):
+        if not self.connection.is_connected():
+            self.connection.reconnect()
+        return self.connection
